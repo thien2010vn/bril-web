bcdd6cc79e2c539a24883d6685bbb04135eafb1e
Thien2010vn <un01677439362@gmail.com>
2025-07-01 16:53:58 +0700

    fix port for Render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def swap():
     return render_template("swap.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Lấy port do Render cung cấp
+    app.run(host='0.0.0.0', port=port, debug=True)  # Mở cho Render truy cập
